13fbd8fe8ebb201c6b5e8b2d7373ef9b22d27a26
aagoins1@gmail.com <>
2014-08-13 19:54:19 +0000

    init

---
+++ js/main.js
@@ -0,0 +1,33 @@
+//initialize Phaser Engine. Create a 400x490px game.
+
+var game = new Phaser.Game(400,490, Phaser.AUTO, "gameDIV")
+
+//Create out 'main' staet that wll contain the game 
+//This is the body of the game itself. It contains all relevant code
+
+var mainState = {
+  
+  preload: function () {
+    //This function will exacture at the begining of the game
+    //Here we'll load all of our assests (art, music, etc)
+    
+},
+  
+  create: function () {
+    //The create funciton is called right after the preload f(x)
+    //This is where we'll set up the game assets from scratch
+  
+},
+  
+  update: function () {
+    //This function is called 60 times a second
+    //It contains the games logic and all time related actions
+    
+}
+  
+ }
+ 
+ 
+ //Add and start the gameState
+ game.state.add('main',mainState);
+ game.state.start('main');
